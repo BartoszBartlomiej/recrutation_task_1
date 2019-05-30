f6a63c691bbc207a825a5ae52ef089d8af5c3dc7
Bartosz <bartosz.lewicki@gmail.com>
2019-05-30 13:17:26 +0200

    Updates

---
+++ precache-manifest.675d86ec094b4ef076617fa4a038449b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709af70b117fe4a2a59541c53a7ab00f",
+    "url": "/recrutation_task_1/index.html"
+  },
+  {
+    "revision": "fb5ebee13e6592222204",
+    "url": "/recrutation_task_1/static/css/main.7047c6bf.chunk.css"
+  },
+  {
+    "revision": "3260db0ce93cef9b5b38",
+    "url": "/recrutation_task_1/static/js/2.91a59ff3.chunk.js"
+  },
+  {
+    "revision": "fb5ebee13e6592222204",
+    "url": "/recrutation_task_1/static/js/main.3f29dca1.chunk.js"
+  },
+  {
+    "revision": "f816629cc55cbdd5ce2a",
+    "url": "/recrutation_task_1/static/js/runtime~main.bcc2ba8e.js"
+  },
+  {
+    "revision": "b9c5c3f43d8f1411dc20b361d2f2b9c1",
+    "url": "/recrutation_task_1/static/media/blok.b9c5c3f4.svg"
+  },
+  {
+    "revision": "a9e6d56fa18d65352f72419fb2d51888",
+    "url": "/recrutation_task_1/static/media/connected.a9e6d56f.png"
+  },
+  {
+    "revision": "1e8824bba0fccdb8af72176706abb60e",
+    "url": "/recrutation_task_1/static/media/form1.1e8824bb.jpg"
+  },
+  {
+    "revision": "4cb4c179f9817a378b9b6289f6a2cc6f",
+    "url": "/recrutation_task_1/static/media/logo.4cb4c179.png"
+  },
+  {
+    "revision": "fbfdcb3ba0a5ca7b042f45e2985e9fcb",
+    "url": "/recrutation_task_1/static/media/metraz.fbfdcb3b.svg"
+  },
+  {
+    "revision": "c6d14f9520e88d9e8dc2bc84023f8de8",
+    "url": "/recrutation_task_1/static/media/samochod.c6d14f95.svg"
+  },
+  {
+    "revision": "d7288184cc4d433030bf943302bc599d",
+    "url": "/recrutation_task_1/static/media/wpis-1-overlay.d7288184.jpg"
+  },
+  {
+    "revision": "4d1f8155385e141fec319e2eb641ed65",
+    "url": "/recrutation_task_1/static/media/wpis-1.4d1f8155.jpg"
+  },
+  {
+    "revision": "633259d2aa134c1c0172bc2501cbf6a4",
+    "url": "/recrutation_task_1/static/media/wpis-2-overlay.633259d2.jpg"
+  },
+  {
+    "revision": "8259f0cc392abb98f12b011dba256697",
+    "url": "/recrutation_task_1/static/media/wpis-2.8259f0cc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/recrutation_task_1/precache-manifest.d49fd02c307c75c5869a6634c726d0a3.js"
+  "/recrutation_task_1/precache-manifest.675d86ec094b4ef076617fa4a038449b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/recrutation_task_1/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recrutation_task_1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
